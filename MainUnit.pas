0755f105024356241512186f4f96666b652711ab
chaosua <chaosua2008@gmail.com>
2014-05-26 20:45:27 +0300

    Truice 1.3.8.1 release for WoTLK 335. Download: http://fbe.am/t1Q

--- MainUnit.pas
+++ MainUnit.pas
@@ -16,7 +16,7 @@ const
   VERSION_1   = '1'; //*10000
   VERSION_2   = '3'; //*100
   VERSION_3   = '8';
-  VERSION_4   = '0';
+  VERSION_4   = '1';
   VERSION_EXE = VERSION_1 + '.' + VERSION_2 + '.' + VERSION_3 + '.' + VERSION_4;
 
   SCRIPT_TAB_NO_QUEST       = 6;
